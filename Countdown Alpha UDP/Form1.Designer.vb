5ffecb1e6eb9a3083813791035bcf91bc8de2aee
alex <alex@DESKTOP-HQAGJKL>
2023-01-20 00:07:28 +0000

    Add project files.

---
+++ Countdown Alpha UDP/Form1.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.LabelPastTime = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 500
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(117, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(107, 29)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Time left"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Panel1
+        '
+        Me.Panel1.AutoSize = True
+        Me.Panel1.BackColor = System.Drawing.Color.Black
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.LabelPastTime)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(8, 8)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(371, 144)
+        Me.Panel1.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Tai Le", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Cornsilk
+        Me.Label2.Location = New System.Drawing.Point(0, 128)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(44, 16)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "RESET"
+        '
+        'LabelPastTime
+        '
+        Me.LabelPastTime.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LabelPastTime.AutoSize = True
+        Me.LabelPastTime.BackColor = System.Drawing.Color.Transparent
+        Me.LabelPastTime.Font = New System.Drawing.Font("Microsoft Tai Le", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelPastTime.ForeColor = System.Drawing.Color.White
+        Me.LabelPastTime.Location = New System.Drawing.Point(8, 25)
+        Me.LabelPastTime.Name = "LabelPastTime"
+        Me.LabelPastTime.Size = New System.Drawing.Size(363, 101)
+        Me.LabelPastTime.TabIndex = 1
+        Me.LabelPastTime.Text = "00:00:00"
+        Me.LabelPastTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(387, 160)
+        Me.Controls.Add(Me.Panel1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(1122, 550)
+        Me.MinimumSize = New System.Drawing.Size(405, 207)
+        Me.Name = "Form1"
+        Me.Padding = New System.Windows.Forms.Padding(8)
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show
+        Me.Text = "Countdown Alpha UDP"
+        Me.TopMost = True
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents LabelPastTime As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ Countdown Alpha UDP/Form1.vb
@@ -0,0 +1,100 @@
+﻿Imports System.Net.Sockets
+Imports System.Threading
+Imports System.Net
+Imports System.Text
+
+Public Class Form1
+    Dim receivingClient As UdpClient
+    Dim receivingThread As Thread
+    Dim timeDown_msg As String
+    Private Delegate Sub WriteMessageDelegate(ByVal msg As String)
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Me.Load
+        'CheckForIllegalCrossThreadCalls = False
+        InitializeReceiver()
+        Call form1_Resize()
+    End Sub
+
+    Private Sub InitializeReceiver()
+        Try
+            receivingClient = New UdpClient(1234)
+            Dim start As ThreadStart = New ThreadStart(AddressOf Receiver)
+            receivingThread = New Thread(start)
+            receivingThread.IsBackground = True
+            receivingThread.Start()
+        Catch ex As Exception
+            MsgBox("Port 1234 is already in use", vbExclamation, "Port in already use")
+        End Try
+    End Sub
+
+    Private Sub Receiver()
+        Dim endPoint As IPEndPoint = New IPEndPoint(IPAddress.Any, 1234)
+        While (True)
+            Dim data() As Byte
+            data = receivingClient.Receive(endPoint)
+            Dim msg As String = Encoding.ASCII.GetString(data)
+            WriteMessage(msg)
+        End While
+    End Sub
+
+    Private Sub WriteMessage(ByVal msg As String)
+        Try
+            If LabelPastTime.InvokeRequired and msg.Length = 8 Then
+                Dim d As New WriteMessageDelegate(AddressOf WriteMessage)
+                LabelPastTime.Invoke(d, New Object() {msg})
+            Else
+                Timer1.Start()
+                LabelPastTime.ForeColor = Color.White
+                LabelPastTime.Text = msg
+            End If
+        Catch ex As Exception
+            Timer1.Stop()
+            MsgBox("Bad time format. Use hh:mm:ss" & vbCrLf & "Example 02:08:05", vbExclamation, "Bad time format 1")
+            Return
+        End Try
+    End Sub
+
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Timer1.Interval = 1000
+        Try
+            timeDown_msg = FormatDateTime(DateAdd(DateInterval.Second, -1, DateTime.Parse(LabelPastTime.Text)), DateFormat.LongTime)
+            LabelPastTime.Text = timeDown_msg
+            If timeDown_msg = "00:00:00" Or timeDown_msg = "23:59:59" Then
+                Timer1.Stop()
+                timeDown_msg = "00:00:00"
+            End If
+        Catch ex As Exception
+            Timer1.Stop()
+            MsgBox("Bad time format. Use hh:mm:ss" & vbCrLf & "Example 02:08:05", vbExclamation, "Bad time format")
+            LabelPastTime.Text = "00:00:00"
+        End Try
+    End Sub
+
+    Private Sub Label2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label2.Click
+        LabelPastTime.Text = "00:00:00"
+        Timer1.Stop()
+    End Sub
+
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As FormClosingEventArgs) Handles Me.FormClosing
+        End
+    End Sub
+
+
+    Sub form1_Resize() Handles Me.Resize
+        Dim PastTimeFontSize As Single
+        PastTimeFontSize = 2 * (Me.Width * 0.07)
+        LabelPastTime.Font = New Font("Microsoft Tai Le", PastTimeFontSize, FontStyle.Bold)
+        LabelPastTime.Location = New Point(LabelPastTime.Parent.Width \ 2 - LabelPastTime.Width \ 2, LabelPastTime.Parent.Height \ 2 - LabelPastTime.Height * 0.4)
+
+        Dim Label1FontSize As Single
+        Label1FontSize = 0.1 * (Me.Width \ 2)
+        Label1.Font = New Font("Microsoft Tai Le", Label1FontSize, FontStyle.Bold)
+
+        Label1.Location = New Point(Label1.Parent.Width \ 2 - Label1.Width \ 2, 0)
+    End Sub
+
+
+End Class
+
+
